b9a12e0394dd85d44538b7d021046435a54b7954
Maxwell Jay <MaxwellJay@jaylab256.onmicrosoft.com>
2023-12-12 21:23:23 +0800

    ver 0.1: transplant source code

--- GDEY0154D67.c
+++ GDEY0154D67.c
@@ -5,6 +5,434 @@
  * @version 0.1
  */
 #include "GDEY0154D67.h"
+
 static const char *TAG = "GDEY0154D67";
 
+static bool epd_is_busy(void);
+static void epd_wait_idle(void);
+static void epd_wait_timeout(int timeout);
+static void epd_refresh_full(void);
+static void epd_refresh_part(void);
+static void epd_refresh_fast(void);
 
+void epd_init_all(void)
+{
+    epd_gpio_init();
+    epd_spi_init();
+    epd_ssd1681_init();
+}
+
+void epd_gpio_init(void)
+{
+    ESP_LOGI(TAG, "Initializing GPIO pins...");
+    // CS, DC, and RST pins
+    gpio_config_t io_conf;
+    io_conf.intr_type = GPIO_INTR_DISABLE; // disable interrupt
+    io_conf.mode = GPIO_MODE_OUTPUT;       // set as output mode
+    io_conf.pin_bit_mask =
+        (1ULL << EPD_CS) | (1ULL << EPD_DC) | (1ULL << EPD_RES);
+    io_conf.pull_down_en = 0; // disable pull-down mode
+    io_conf.pull_up_en = 0;   // disable pull-up mode
+    gpio_config(&io_conf);
+
+    // Busy pin
+    io_conf.intr_type = GPIO_INTR_NEGEDGE;     // interrupt on negative edge
+    io_conf.mode = GPIO_MODE_INPUT;            // set as input mode
+    io_conf.pin_bit_mask = (1ULL << EPD_BUSY); // apply to busy pin
+    io_conf.pull_up_en = 1;                    // enable pull-up mode
+    gpio_config(&io_conf);
+
+    ESP_LOGI(TAG, "GPIO pins initialized.");
+}
+
+void epd_ssd1681_init(void)
+{
+    ESP_LOGI(TAG, "Initializing SSD1681...");
+
+    gpio_set_level(EPD_RES, 0); // Reset module
+    vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_set_level(EPD_RES, 1); // Release reset
+    vTaskDelay(pdMS_TO_TICKS(10));
+
+    epd_wait_idle();
+    epd_spi_send_command(0x12); // Soft reset
+    epd_wait_idle();
+
+    epd_spi_send_command(0x01); // Driver output control
+    epd_spi_send_data(0xC7);    // Gate scan direction: GS0 -> GS63
+    epd_spi_send_data(0x00);    // Source shift direction: S0 -> S199
+    epd_spi_send_data(0x01);    // Booster switch: on
+
+    epd_spi_send_command(0x11); // Data entry mode setting
+    epd_spi_send_data(0x01);    // X increment, Y increment
+
+    epd_spi_send_command(0x44); // Set RAM X start/end address
+    epd_spi_send_data(0x00);    // RAM x address start at 00h;
+    epd_spi_send_data(0x18);    // RAM x address end at 18h(24+240-1=263);
+
+    epd_spi_send_command(0x45); // Set RAM Y start/end address
+    epd_spi_send_data(0xC7);    // RAM y address start at C7h;
+    epd_spi_send_data(0x00);    // RAM y address end at 00h(320-1=319);
+    epd_spi_send_data(0x00);
+    epd_spi_send_data(0x00);
+
+    epd_spi_send_command(0x3C); // Border waveform
+    epd_spi_send_data(0x05);    // Border floating
+
+    epd_spi_send_command(0x18); // Temperature sensor control
+    epd_spi_send_data(0x80);    // Internal temperature sensor
+
+    epd_spi_send_command(0x4E); // Set RAM x address count to 0;
+    epd_spi_send_data(0x00);
+    epd_spi_send_command(0x4F); // Set RAM y address count to 0X199;
+    epd_spi_send_data(0xC7);
+    epd_spi_send_data(0x00);
+    epd_wait_idle();
+
+    ESP_LOGI(TAG, "SSD1681 initialized.");
+}
+
+/**
+ * @brief Check if epaper is busy
+ * @return true-busy; false-idle
+ */
+static bool epd_is_busy(void)
+{
+    return gpio_get_level(EPD_BUSY);
+}
+
+/**
+ * @brief Wait until epaper is idle
+ */
+static void epd_wait_idle(void)
+{
+    while (1)
+    {
+        if (epd_is_busy() == false)
+            break;
+    }
+}
+
+/**
+ * @brief Wait until epaper is idle or timeout
+ * @param timeout Timeout in ms
+ */
+static void epd_wait_timeout(int timeout)
+{
+    while (timeout > 0)
+    {
+        if (epd_is_busy() == false)
+            break;
+        vTaskDelay(pdMS_TO_TICKS(100));
+        timeout -= 100;
+    }
+    if (timeout <= 0)
+    {
+        ESP_LOGE(TAG, "Timeout after %dms.", timeout);
+    }
+}
+
+void epd_deep_sleep(void)
+{
+    epd_spi_send_command(0x10);
+    epd_spi_send_data(0x01); // Enter deep sleep mode 1
+    vTaskDelay(pdMS_TO_TICKS(100));
+}
+
+/*Refresh functions*/
+/**
+ * @brief Refresh the screen using full update mode
+ */
+static void epd_refresh_full(void)
+{
+    ESP_LOGD(TAG, "Refreshing(full)...");
+    epd_spi_send_command(0x22); // Display update control 2
+    epd_spi_send_data(0xF7);    // Load temperature and waveform setting
+    epd_spi_send_command(0x20); // Activate display update sequence
+    epd_wait_timeout(1000);     // Wait at most 1s
+}
+/**
+ * @brief Refresh the screen using partial update mode
+ */
+static void epd_refresh_part(void)
+{
+    ESP_LOGD(TAG, "Refreshing(partial)...");
+    epd_spi_send_command(0x22); // Display update control 2
+    epd_spi_send_data(0xFF);    // Load temperature and waveform setting
+    epd_spi_send_command(0x20); // Activate display update sequence
+    epd_wait_timeout(1000);     // Wait at most 1s
+}
+/**
+ * @brief Refresh the screen using fast update mode
+ */
+static void epd_refresh_fast(void)
+{
+    ESP_LOGD(TAG, "Refreshing(fast)...");
+    epd_spi_send_command(0x22); // Display update control 2
+    epd_spi_send_data(0xC7);    // C7:
+    epd_spi_send_command(0x20); // Activate display update sequence
+    epd_wait_timeout(100);      // Wait at most 100ms
+}
+
+void epd_clear_screen(uint8_t color)
+{
+    uint16_t i;
+    epd_spi_send_command(0x24); // Write RAM for black(0)/white (1)
+    for (i = 0; i < EPD_SCREEN_HEIGHT * EPD_SCREEN_WIDTH / 8; ++i) {
+        epd_spi_send_data(color); // bit set: white, bit reset: black
+    }
+    epd_refresh_full();
+}
+
+/**
+ * @brief Full refresh with a given data buffer
+ * @param data Data to display
+ */
+void epd_print_full_bydata(const uint8_t *data)
+{
+    unsigned int i;
+    epd_spi_send_command(0x24); // Write RAM for black(0)/white (1)
+    for (i = 0; i < EPD_DATA_LEN; ++i)
+        epd_spi_send_data(*data++);
+}
+
+/**
+ * @brief Full refresh with an image_display function(without parameters)
+ * @param image_display Function to display image
+ */
+void epd_print_full_byfunction(void image_display(void))
+{
+    epd_ssd1681_init();
+    image_display(); // display image
+    epd_refresh_full();
+    epd_deep_sleep(); // enter deep sleep
+}
+
+/**
+ * @brief Full refresh with an image_display function(with data)
+ * @param display_func Function to display image
+ * @param data Data to display
+ */
+void epd_print_full(
+    void display_func(const uint8_t *data), const uint8_t *data)
+{
+    epd_ssd1681_init();
+    display_func(data); // display image
+    epd_refresh_full();
+    epd_deep_sleep(); // enter deep sleep
+}
+
+/**
+ * @brief Set RAM address range (for partial refresh)
+ * @note Used in epd_print_part_byfunction() and epd_print_part()
+ * @param x_start X start position
+ * @param y_start Y start position
+ * @param x_size X size
+ * @param y_size Y size
+ */
+static void epd_partial_set_RAM_address(
+    uint16_t x_start, uint16_t y_start, uint16_t x_size, uint16_t y_size)
+{
+    uint16_t x_end, y_start1, y_start2, y_end1, y_end2;
+    x_start = x_start / 8;
+    x_end = x_start + x_size / 8 - 1;
+
+    y_start1 = 0; // y_start1 should be the multiple of 8, since the basic element is 8 pixels.
+    y_start2 = EPD_SCREEN_HEIGHT - y_start; // The last line of the image should be the last line of the last data byte
+    if (y_start >= 256) {
+        y_start1 = y_start2 / 256;
+        y_start2 = y_start2 % 256;
+    }
+    y_end1 = 0;
+    y_end2 = y_start2 + y_size - 1;
+    if (y_end2 >= 256) {
+        y_end1 = y_end2 / 256;
+        y_end2 = y_end2 % 256;
+    }
+
+    epd_spi_send_command(0x44); // Set RAM X start/end address
+    epd_spi_send_data(x_start); // RAM x address start at 00h;
+    epd_spi_send_data(x_end);   // RAM x address end at 18h(24+240-1=263);
+    epd_spi_send_command(0x45); // Set RAM Y start/end address
+    epd_spi_send_data(y_start2); // RAM y address start at C7h;
+    epd_spi_send_data(y_start1); // RAM y address start at C7h;
+    epd_spi_send_data(y_end2);   // RAM y address end at 00h(320-1=319);
+    epd_spi_send_data(y_end1);   // RAM y address end at 00h(320-1=319);
+
+    epd_spi_send_command(0x4E); // Set RAM x address count to 0;
+    epd_spi_send_data(x_start);
+    epd_spi_send_command(0x4F); // Set RAM y address count to 0X127;
+    epd_spi_send_data(y_start2);
+    epd_spi_send_data(y_start1);
+}
+
+/**
+ * @brief Partial refresh with an image_display function (without parameters)
+ * @param x_start X start position
+ * @param y_start Y start position
+ * @param x_size X size
+ * @param y_size Y size
+ * @param image_display Function to display image
+ */
+void epd_print_part_byfunction(
+    uint16_t x_start, uint16_t y_start,
+    uint16_t x_size, uint16_t y_size,
+    void image_display(void))
+{
+    ESP_LOGD(TAG, "Partial refresh at x_start=%d, x_end=%d, y_start=%d, y_end=%d",
+             x_start, x_start + x_size - 1, y_start, y_start + y_size - 1);
+    // Add hardware reset to prevent background color change
+    gpio_set_level(EPD_RES, 0); // Reset module
+    vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_set_level(EPD_RES, 1); // Release reset
+    vTaskDelay(pdMS_TO_TICKS(10));
+
+    // Lock the border to prevent accidental refresh
+    epd_spi_send_command(0x3C); // Border waveform
+    epd_spi_send_data(0x80);
+
+    epd_partial_set_RAM_address(x_start, y_start, x_size, y_size);
+
+    epd_spi_send_command(0x24); // Write RAM for black(0)/white (1)
+    image_display();
+
+    epd_refresh_part();
+    epd_deep_sleep();
+}
+
+/**
+ * @brief Partial refresh with an image_display function (with data)
+ * @param x_start X start position
+ * @param y_start Y start position
+ * @param x_size X size
+ * @param y_size Y size
+ * @param display_func Function to display image
+ * @param data Data to display
+ */
+void epd_print_part(
+    uint16_t x_start, uint16_t y_start,
+    uint16_t x_size, uint16_t y_size,
+    void display_func(const uint8_t *data), const uint8_t *data)
+{
+    ESP_LOGD(TAG, "Partial refresh at x_start=%d, x_end=%d, y_start=%d, y_end=%d",
+             x_start, x_start + x_size - 1, y_start, y_start + y_size - 1);
+    // Add hardware reset to prevent background color change
+    gpio_set_level(EPD_RES, 0); // Reset module
+    vTaskDelay(pdMS_TO_TICKS(10));
+    gpio_set_level(EPD_RES, 1); // Release reset
+    vTaskDelay(pdMS_TO_TICKS(10));
+
+    // Lock the border to prevent accidental refresh
+    epd_spi_send_command(0x3C); // Border waveform
+    epd_spi_send_data(0x80);
+
+    epd_partial_set_RAM_address(x_start, y_start, x_size, y_size);
+
+    epd_spi_send_command(0x24); // Write RAM for black(0)/white (1)
+    display_func(data);
+
+    epd_refresh_part();
+    epd_deep_sleep();
+}
+
+/*Experimental functions, not available for use!*/
+
+/// @brief 2-dimension array to store partial data
+uint8_t partial_data[EPD_SCREEN_HEIGHT][EPD_SCREEN_WIDTH / 8];
+
+/// @brief 1-dimension array to store partial data
+uint8_t partial_data_array[EPD_SCREEN_HEIGHT * EPD_SCREEN_WIDTH / 8];
+
+/**
+ * @brief Initialize partia_data and partial_data_array with all 0xff(white)
+ */
+void epd_partial_data_init(void)
+{
+    memset(partial_data, EPD_WHITE, sizeof(partial_data));
+    memset(partial_data_array, EPD_WHITE, sizeof(partial_data_array));
+}
+
+/**
+ * @brief Add partial data to partial_data_array
+ * @param x_start X start position
+ * @param x_end X end position
+ * @param y_start Y start position
+ * @param y_end Y end position
+ * @param data Data to add
+ */
+void epd_partial_data_add(
+    uint16_t x_start, uint16_t x_end,
+    uint16_t y_start, uint16_t y_end,
+    const uint8_t *data)
+{
+    uint8_t x_len = x_end - x_start;
+    uint8_t x_data_location = x_start / 8;
+    uint8_t x_size = x_len / 8;
+
+    int data_index = 0;
+    if (x_start % 8 != 0) {
+        x_data_location++;
+    }
+    if (x_len % 8 != 0) {
+        x_size++;
+    }
+
+    for (int x_index = y_start; x_index < y_end; ++x_index) {
+        for (int y_index = x_data_location; y_index < (x_data_location + x_size); ++y_index) {
+            partial_data[x_index][y_index] = (~data[data_index++]); // ~: black->white, white->black
+        }
+    }
+}
+
+/**
+ * @brief Pass partial_data_array to epaper
+ */
+static void epd_print_partial_data(void)
+{
+    uint16_t i;
+    epd_spi_send_command(0x24);
+    for (i = 0; i < EPD_DATA_LEN; ++i) {
+        epd_spi_send_data(partial_data_array[i]);
+    }
+
+    epd_spi_send_command(0x26);
+    for (i = 0; i < EPD_DATA_LEN; ++i) {
+        epd_spi_send_data(partial_data_array[i]);
+    }
+}
+
+/**
+ * @brief Copy partial_data to partial_data_array
+ */
+void epd_partial_data_copy(void)
+{
+    int data_index = 0;
+    for (int i = 0; i < EPD_SCREEN_HEIGHT; i++) {
+        for (int j = 0; j < EPD_SCREEN_WIDTH / 8; j++) {
+            partial_data_array[data_index++] = partial_data[i][j];
+        }
+    }
+    epd_print_full_byfunction(epd_print_partial_data);
+}
+
+/**
+ * @brief Set RAM value for base map
+ * @param image_buffer Image buffer
+ */
+void epd_setRAMvalue_BaseMap(const uint8_t *image_buffer)
+{
+    uint16_t i;
+    const uint8_t *data_flag;
+    data_flag = image_buffer;
+
+    epd_spi_send_command(0x24); // Write RAM for black(0)/white (1)
+    for (i = 0; i < EPD_DATA_LEN; ++i)
+        epd_spi_send_data(*image_buffer++);
+    image_buffer = data_flag;
+
+    epd_spi_send_command(0x26); // Write RAM for black(0)/white (1)
+    for (i = 0; i < EPD_DATA_LEN; ++i)
+        epd_spi_send_data(*image_buffer++);
+
+    epd_refresh_full();
+}
--- GDEY0154D67.h
+++ GDEY0154D67.h
@@ -7,13 +7,105 @@
 #ifndef _GDEY0154D67_H_
 #define _GDEY0154D67_H_
 
-#include "driver/spi_common.h"
-#include "driver/spi_master.h"
-#include "driver/gpio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "esp_log.h"
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "GDEY0154D67_spi.h"
 
-#define GDEY0154D67_WIDTH 200
-#define GDEY0154D67_HEIGHT 200
+// IO settings
+#define EPD_BUSY GPIO_NUM_13 // Busy pin, 1-busy 0-idle
+#define EPD_RES GPIO_NUM_12  // Reset pin, 1-normal 0-reset
+#define EPD_DC GPIO_NUM_14   // Data/Command, 1-data 0-command
+#define EPD_CS GPIO_NUM_27   // Chip Select, 1-inactive 0-active
+// SPI settings
+#define EPD_SPI_MISO GPIO_NUM_23 // MISO signal
+#define EPD_SPI_MOSI GPIO_NUM_26 // MOSI signal
+#define EPD_SPI_CLK GPIO_NUM_25  // CLK signal
+
+#define EPD_SCREEN_WIDTH 200  // Width of epaper
+#define EPD_SCREEN_HEIGHT 200 // Height of epaper
+#define EPD_DATA_LEN EPD_SCREEN_WIDTH * EPD_SCREEN_HEIGHT / 8 // Total length of an image data array
+#define EPD_WHITE 0xff // White pixel
+#define EPD_BLACK 0x00 // Black pixel
+
+///////Initialize functions///////
+/**
+ * @brief Initialize epaper, including gpio, spi and SSD1681
+ */
+void epd_init_all(void);
+
+/**
+ * @brief Initialize epaper gpio pins(apart from spi)
+ */
+void epd_gpio_init(void);
+
+/**
+ * @brief Initialize epaper IC - SSD1681
+*/
+void epd_ssd1681_init(void);
+
+///////Basic display functions///////
+/**
+ * @brief Clear the screen with black / white
+ * @param color EPD_WHITE-white, EPD_BLACK-black
+ */
+void epd_clear_screen(uint8_t color);
+
+/**
+ * @brief Enter deep sleep mode
+*/
+void epd_deep_sleep(void);
+
+///////Full display functions///////
+/**
+ * @brief Full refresh with a given data buffer
+ * @param image_buffer Image buffer
+ */
+void epd_print_full_bydata(const uint8_t *data);
+
+/**
+ * @brief Full refresh with an image_display function(without parameters)
+ * @param image_display Function to display image
+ */
+void epd_print_full_byfunction(void image_display(void));
+
+/**
+ * @brief Full refresh with an image_display function(with data)
+ * @param display_func Function to display image
+ * @param data Data array to display
+ */
+void epd_print_full(void display_func(const uint8_t *data), const uint8_t *data);
+
+///////Partial display functions///////
+/**
+ * @brief Partial refresh with an image_display function (without parameters)
+ * @param x_start X start position
+ * @param y_start Y start position
+ * @param x_size X size
+ * @param y_size Y size
+ * @param image_display Function to display image
+ */
+void epd_print_part_byfunction(
+    uint16_t x_start, uint16_t y_start,
+    uint16_t x_size, uint16_t y_size,
+    void image_display(void));
+
+/**
+ * @brief Partial refresh with an image_display function (with data)
+ * @param x_start X start position
+ * @param y_start Y start position
+ * @param x_size X size
+ * @param y_size Y size
+ * @param display_func Function to display image
+ * @param data Data to display
+ */
+void epd_print_part(
+    uint16_t x_start, uint16_t y_start,
+    uint16_t x_size, uint16_t y_size,
+    void display_func(const uint8_t *data), const uint8_t *data);
 
 #endif // _GDEY0154D67_H_
